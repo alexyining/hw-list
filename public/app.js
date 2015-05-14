f9495540175e52727288c1c47de68be3763fad85
Alex Chen <y.chen@columbia.edu>
2015-05-13 23:53:36 -0400

    complete

--- public/app.js
+++ public/app.js
@@ -1,1 +1,35 @@
 var app = angular.module('HwList', []);
+
+app.factory('Homework', function() {
+	var data = [
+		{
+			assignment: 'Test First Javascript',
+			url: 'https://github.com/alexyining/FullstackTestFirst',
+			status: 'completed'
+		},
+
+		{
+			assignment: 'Angular Day 1',
+			url: 'https://github.com/alexyining/flashcards-day1',
+			status: 'completed'
+		},
+
+		{
+			assignment: 'Angular Day 2',
+			url: 'https://github.com/alexyining/flashcards-day2',
+			status: 'completed'
+		}
+	];
+
+	return data;
+});
+
+app.controller('MainController', function($scope, Homework) {
+	$scope.hwlist = Homework;
+});
+
+app.controller('HwController', function($scope) {
+	$scope.click = function() {
+		$scope.clicked=true;
+	}
+})
--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 var server = require('./server');
 
-var PORT = 1337;
+var PORT = 8888;
 
 server.listen(PORT, function () {
     console.log('Server started on port ' + PORT.toString());
